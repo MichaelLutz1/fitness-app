786b88dd83358c550b97845433d3f4e0a31a1618
Michael Lutz <mikelutz2400@gmail.com>
2022-12-30 02:17:16 -0500

    show sign in id

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,16 +1,19 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../firebase";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState();
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    signInWithPopup(auth, provider).then((value) => {
+      setUser(value.user.uid);
+    });
   };
   return (
-    <AuthContext.Provider value={{ googleSignIn }}>
+    <AuthContext.Provider value={{ googleSignIn: googleSignIn, user: user }}>
       {children}
     </AuthContext.Provider>
   );
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -3,11 +3,13 @@ import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../contexts/AuthContext";
 
 function Login() {
-  const { googleSignIn } = UserAuth();
+  const context = UserAuth();
+  const googleSignIn = context.googleSignIn;
   return (
     <>
       <h1>Login Page</h1>
       <GoogleButton onClick={() => googleSignIn()} />
+      <h1>{context.user ? context.user : null}</h1>
     </>
   );
 }
