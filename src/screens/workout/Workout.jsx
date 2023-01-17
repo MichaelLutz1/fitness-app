d2e4cfed28dc7e787614d1baa5c53fcf424efdf5
Michael Lutz <mikelutz2400@gmail.com>
2023-01-17 17:01:48 -0500

    exercise variable into state

--- src/screens/workout/Workout.jsx
+++ src/screens/workout/Workout.jsx
@@ -1,32 +1,60 @@
 import { useState } from "react";
 import { ExerciseCard } from "./ExerciseCard";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function Workout() {
-  let exercises = [];
   const [isEditting, setIsEditting] = useState(null);
-  exercises.push({name:'bob'})
-  exercises.push({name:'kevin'})
-//  {name:
-//  reps:
-//  weight:
-//  lastReps:
-//  lastWeight:
-//  }
+  const [exercises, setExercises] = useState([]);
+  // Exercise --
+  //  {name:
+  //  reps:
+  //  weight:
+  //  lastReps:
+  //  lastWeight:
+  //  }
+  function handleClick(name) {
+    setExercises([...exercises, { name: name }]);
+  }
 
-  return (<>
-  <h1>Workout page</h1>
-  {exercises.map((ex, i) => <ExerciseCard name ={ex.name} setIsEditting={setIsEditting} index = {i} key={ex.name}>{ex}</ExerciseCard>)}
+  return (
+    <>
+      <h1>Workout page</h1>
+      {exercises.map((ex, i) => (
+        <ExerciseCard
+          name={ex.name}
+          setIsEditting={setIsEditting}
+          index={i}
+          key={ex.name}
+        >
+          {ex}
+        </ExerciseCard>
+      ))}
 
-  <div className="addBtn">chad add button
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      <Dropdown.Item onClick={()=>{console.log("asfdlkjal;kjfasdl;kj")}}>Action</Dropdown.Item>
-      <Dropdown.Item>Another action</Dropdown.Item>
-      <Dropdown.Item onClick={() => (console.log('clicked'))} >Something else</Dropdown.Item>
-    </DropdownButton>
-  </div>
-  </>);
+      <div className="addBtn">
+        chad add button
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item
+            onClick={() => {
+              handleClick("Shoulder Press");
+            }}
+          >
+            Shoulder Press
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              handleClick("Squat");
+            }}
+          >
+            Shoulder Press
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleClick("BenchPress")}>
+            Bench Press
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+    </>
+  );
 }
 
 export default Workout;
